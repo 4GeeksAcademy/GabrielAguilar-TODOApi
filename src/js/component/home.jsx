f3dad75f341e9dd69624d46012d89071572e70d3
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-11-26 20:34:51 +0000

    todolist react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,170 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import "../../styles/index.css";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+  
+	useEffect(() => {
+	  createUser()
+	  takeList()
+  
+	}, [])
+  
+	useEffect(() => {
+  
+	  sendList()
+  
+	}, [todos])
+  
+	const createUser = async () => {
+  
+	  try {
+		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Gabo1', {
+  
+		  method: "POST",
+		  body: JSON.stringify([]),
+		  headers: {
+			"Content-Type": "application/json"
+		  }
+		})
+  
+		if (!response.ok) {
+		  throw Error(response.statusText);
+		}
+  
+		const transform = await response.json()
+		console.log(transform.msg)
+  
+		return transform
+	  }
+  
+	  catch (e) {
+  
+	  }
+  
+	}
+  
+	const takeList = async () => {
+  
+	  try {
+  
+		const previewResponse = await fetch(
+		  'https://playground.4geeks.com/apis/fake/todos/user/Gabo1'
+		)
+  
+		if (!previewResponse.ok) {
+		  throw Error(previewResponse.statusText);
+		}
+  
+		const transform = await previewResponse.json()
+		setTodos(transform)
+  
+	  }
+  
+	  catch (e) {
+  
+		console.log("error", e)
+	  }
+  
+	}
+  
+	const sendList = async () => {
+  
+	  try {
+		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Gabo1', {
+  
+		  method: "PUT",
+		  body: JSON.stringify(todos),
+		  headers: {
+			"Content-Type": "application/json"
+		  }
+  
+		})
+  
+		if (!response.ok) {
+		  throw Error(response.statusText);
+		}
+  
+		const transform = await response.json()
+		console.log(transform.msg)
+  
+		return transform
+	  }
+  
+	  catch (e) {
+  
+	  }
+  
+	}
+  
+	const pressEnter = async (e) => {
+	  try {
+		if (e.key === "Enter" && inputValue !== "") {
+  
+		  let obj = {
+			label: inputValue,
+			done: false
+		  }
+  
+		  setTodos([...todos, obj]);
+		  setInputValue("");
+  
+		  const final = await sendList()
+		  console.log("informacion enviada", final)
+  
+		}
+	  }
+	  catch (e) {
+		console.log("Hubo un error", e)
+	  }
+  
+	}
+  
+	return (
+  
+	  <div className="Container">
+  
+		<h1 className="title">Todos</h1>
+  
+		<ul className="list-group list-group-flush">
+  
+		  <input
+			type="text"
+			onChange={(e) => setInputValue(e.target.value)}
+			value={inputValue}
+			onKeyDown={(e) => pressEnter(e)}
+			placeholder="What do you need to do?" />
+  
+		  {todos.map((item, index) => (
+  
+			<li className="list-group-item li-c" key={index}>
+  
+			  {item.label}
+  
+			  <button onClick={() => {
+				const updatedTodos = todos.filter(
+				  (_, currentIndex) => index != currentIndex
+				);
+  
+				setTodos(updatedTodos);
+  
+			  }}>  <i
+				className="fas fa-solid fa-trash"
+			  ></i></button>
+  
+			</li>
+		  ))}
+  
+		</ul>
+  
+		<div className="items">{todos.length} items left</div>
+  
+	  </div>
+	);
+  };
+  
+  export default Home;
